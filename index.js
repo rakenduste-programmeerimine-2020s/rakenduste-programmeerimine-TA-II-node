58687b21be374d688d2fe47ba2fcc55fa428903d
Romil Rõbtšenkov <romil@testlio.com>
2020-09-06 15:14:26 +0300

    second lesson

--- index.js
+++
@@ -1,2 +0,0 @@
-console.log('Hello world');
-require('net').createServer().listen();
---
+++ src/models/topic.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const TopicSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        viewCount: { type: Number, default: 0 }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('Topic', TopicSchema)
