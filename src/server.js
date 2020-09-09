23008ff9fc4f89e045a1d5d4c208a37d86d40320
marisjo <marisjool@gmail.com>
2020-09-09 12:19:03 +0300

    Muudatused

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+const mongoose = require('mongoose');
+
+const router = require('./routes');
+
+
+const app = new Koa();
+app.use(require('koa-body')());
+app.use(router.routes());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        const listener = app.listen(process.env.APP_PORT || 3000, () =>
+            console.log('App started on port ' + listener.address().port)
+        )
+    })
+    .catch(() => {
+        console.error('Unable to connect to MongoDB')
+        process.exit(1)
+    })
+
+//    app.proxy = true;
+module.exports = app;
+
