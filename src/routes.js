26e20948ab5764ecee52f5a097df36e05357abfc
hp355837 <55134673+hp355837@users.noreply.github.com>
2020-09-15 15:00:47 +0300

    MongolsDB Post via postman setupt

---
+++ src/routes.js
@@ -0,0 +1,57 @@
+
+const Router = require('koa-router');
+
+const router = new Router()
+
+const Topic = require('./models/topic')
+
+router.get('/health', (context) => {
+    console.log('korras')
+    context.status = 200;
+    context.body = 'OK';
+});
+
+router.get('/topics/:id', async (context) => {
+    const {name} = context.body;
+    const topics = await Topic.findOne({name: {name}});
+
+    context.status = 200;
+    context.body = topics
+
+    return;
+});
+
+router.put('/topics/update', async (ctx) => {
+    console.log(ctx.request.body.name);
+    await Topic.findByIdAndUpdate({_id: ctx.request.body._id}, {name: ctx.request.body.name})
+    .catch((error) => {
+        console.log(error)
+    })
+})
+   
+
+router.get('/topics', async (context) => {
+    const topics = await Topic.find({});
+
+    context.status = 200;
+    context.body = topics
+
+    return;
+})
+
+router.post('/topics', async (ctx) => {
+    console.log(ctx.request.body)
+    const { name } = ctx.request.body
+    let newTopic = new Topic({name})
+
+    newTopic.save()
+            .then(() => {
+                console.log("Andmed salvestatud")
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    return
+})
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,26 @@
+const Koa = require('koa');
+const mongoose = require('mongoose');
+const router = require('./routes');
+
+
+const app = new Koa();
+
+app.use(require('koa-body')({multipart:true}))
+app.use(router.routes());
+
+
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        const listener = app.listen(process.env.APP_PORT || 3000, () =>
+            console.log('App started on port ' + listener.address().port)
+        )
+    })
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
+
+
+app.proxy = true;
+module.exports = app;
