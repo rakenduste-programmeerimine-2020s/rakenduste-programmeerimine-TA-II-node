e4c25b3c9f284eb910d4577e083ecde21cf40814
filiptaik <filip.taik@hotmail.com>
2020-09-16 11:36:19 +0300

    llintimine ja kerge validation

--- src/routes.js
+++ src/routes.js
@@ -1,65 +1,81 @@
-
-const Router = require('koa-router');
-
+const Router = require('koa-router')
+const Joi = require('Joi')
+const validate = require('koa-joi-validate')
 const router = new Router()
 
 const Topic = require('./models/topic')
 
+const postValidator = validate({
+  body: {
+    name: Joi.string().required()
+  }
+})
 router.get('/health', (context) => {
-    console.log('korras')
-    context.status = 200;
-    context.body = 'OK';
-});
+  console.log('korras')
+  context.status = 200
+  context.body = 'OK'
+})
 
 router.get('/topics/:id', async (context) => {
-    const {name} = context.body;
-    const topics = await Topic.findOne({name: {name}});
+  console.log(context.route)
+  context.body = context.params.id
+  const topics = await Topic.findOne({ _id: context.body })
 
-    context.status = 200;
-    context.body = topics
-
-    return;
-});
-
-router.put('/topics/update/:id', async (ctx) => {
-    await console.log(ctx);
-    Topic.findByIdAndUpdate({id: ctx.request.body._id}, {name: ctx.request.body.name})
-    .catch((error) => {
-        console.log(error)
-    })
+  context.status = 200
+  context.body = topics
 })
-   
+
+router.put('/topics/update/', async (ctx) => {
+  console.log(ctx.request.body.name)
+  await Topic.findByIdAndUpdate(
+    { _id: ctx.request.body._id },
+    { name: ctx.request.body.name }
+  ).catch((error) => {
+    console.log(error)
+  })
+})
 
 router.get('/topics', async (context) => {
-    const topics = await Topic.find({});
+  const topics = await Topic.find({})
 
-    context.status = 200;
-    context.body = topics
-
-    return;
+  context.status = 200
+  context.body = topics
 })
 
 router.post('/topics', async (ctx) => {
-    console.log(ctx)
+  console.log(ctx.request.body)
+  const { name } = ctx.request.body
+  const newTopic = new Topic({ name })
 
-    const { name } = ctx.request.body
-    newPost = {
-        name: {name}
-    }
+  const topic = await newTopic.save() // asünkroonne
+  /* sünkroonne
 
-    let newTopic = new Topic(newPost)
-    
-   
-    newTopic.save()
-            .then(() => {
+            .then((topic) => {
                 console.log("Andmed salvestatud")
+                ctx.status = 201
+                ctx.body = topic
             })
             .catch((error) => {
                 console.log(error)
             })
-       
-
-    return;
+            */
+  ctx.status = 201
+  ctx.body = topic
 })
 
-module.exports = router;
+router.del('/topics/:id', async (ctx) => {
+  ctx.body = ctx.params.id
+  // let deleteTopic = new Topic({_id})
+  Topic.deleteOne({ _id: ctx.body })
+    .then(() => {
+      console.log(ctx.body)
+      console.log('Andmed kustutatud')
+    })
+    .catch((error) => {
+      console.log(error)
+      ctx.status = 404
+    })
+  ctx.status = 200
+})
+
+module.exports = router
