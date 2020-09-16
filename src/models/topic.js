69f9dbe3d1a2eb30182680b6875dbd461480577b
Romil Rõbtšenkov <romil@testlio.com>
2020-09-16 11:21:44 +0300

    added eslint and debug

--- src/models/topic.js
+++ src/models/topic.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const TopicSchema = new Schema(
-    {
-        id: {},
-        name: { type: String, required: true },
-        viewCount: { type: Number, default: 0 }
-    },
-    {
-        timestamps: true
-    }
+  {
+    id: {},
+    name: { type: String, required: true },
+    viewCount: { type: Number, default: 0 }
+  },
+  {
+    timestamps: true
+  }
 )
 
 module.exports = mongoose.model('Topic', TopicSchema)
--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,25 @@
 
-const Router = require('koa-router');
+const Router = require('koa-router')
 
 const router = new Router()
 
 const Topic = require('./models/topic')
 
 router.get('/topics', async (context) => {
-    const topics = await Topic.find({});
+  const topics = await Topic.find({})
 
-    context.status = 200;
-    context.body = topics
-
-    return;
+  context.status = 200
+  context.body = topics
 })
 
 router.post('/topics', async (context) => {
-    const { name } = context.request.body
+  const { name } = context.request.body
 
-    const newTopic = new Topic({ name })
-    const topic = await newTopic.save()
+  const newTopic = new Topic({ name })
+  const topic = await newTopic.save()
 
-    context.status = 201;
-    context.body = topic;
+  context.status = 201
+  context.body = topic
+})
 
-    return;
-});
-
-module.exports = router;
+module.exports = router
