706e1b246dff9d80eaeee1090f71e311e95428bb
oliko888 <oliverkobing@gmail.com>
2020-09-20 14:05:47 +0300

    Kodutoo

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-console.log('Hello world');
+console.log('Hello world!!');
 require('net').createServer().listen();
---
+++ src/routes.js
@@ -0,0 +1,35 @@
+const Router = require('koa-router');
+const router = new Router();
+const Topic = require('./models/topic')
+
+router.get('/health', (context) => {
+    context.status = 200;
+    context.body = 'OK';
+});
+
+router.get('/topics', async (context) => {
+    const topics = await Topic.find({});
+
+    context.status = 200;
+    context.body = topics
+
+    return;
+});
+
+router.post('/topics', async (context) => {
+    const {
+        name
+    } = context.request.body
+
+    const newTopic = new Topic({
+        name
+    })
+    const topic = await newTopic.save()
+
+    context.status = 201;
+    context.body = topic;
+
+    return;
+});
+
+module.exports = router;
