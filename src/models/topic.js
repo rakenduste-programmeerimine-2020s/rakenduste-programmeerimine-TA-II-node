3ede72ffafb01e997b7474c35622983d1eddc14a
Sandra Maidla <sandramai@users.noreply.github.com>
2020-09-22 17:49:44 +0300

    Added Joi

--- src/models/topic.js
+++ src/models/topic.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const TopicSchema = new Schema(
-    {
-        id: {},
-        name: { type: String, required: true },
-        viewCount: { type: Number, default: 0 }
-    },
-    {
-        timestamps: true
-    },
+  {
+    name: { type: String, required: true },
+    viewCount: { type: Number, default: 0 },
+    age: { type: Number },
+    email: { type: String }
+  },
+  {
+    timestamps: true
+  }
 )
 
 module.exports = mongoose.model('two/Topic', TopicSchema)
--- src/routes.js
+++ src/routes.js
@@ -1,58 +1,69 @@
-const Router = require('koa-router');
-const router = new Router();
+const Router = require('koa-router')
+const Joi = require('joi')
+const router = new Router()
 const Topic = require('./models/topic')
 
 router.get('/topics/:id', async (context) => {
-    context.body = context.params.id;
-    const topics = await Topic.findOne({_id: context.body});
-
-    context.status = 200;
-    context.body = topics;
-
-    return;
-});
+  try {
+    context.body = context.params.id
+    const topics = await Topic.findOne({ _id: context.body })
+    context.status = 200
+    context.body = topics
+  } catch (e) {
+    context.status = 400
+    context.body = e
+  }
+})
 
 router.get('/topics', async (context) => {
-    const topics = await Topic.find({});
+  const topics = await Topic.find({})
 
-    context.status = 200;
-    context.body = topics
-
-    return;
-});
+  context.status = 200
+  context.body = topics
+})
 
 router.put('/topics/:id', async (context) => {
+  try {
+    const _id = context.params.id
+    const { name, age, email } = context.request.body
+    const schema = Joi.object({
+      name: Joi.string().min(3).max(20),
+      age: Joi.number().integer(),
+      email: Joi.string().min(10).max(30)
+    })
+    const { error } = schema.validate({ name: name, age: age, email: email })
 
-    try {
-        const _id =  context.params.id;
-        const topic = await Topic.findOne({ _id})
-        const updates = Object.keys(context.request.body)
-        
-        if (!topic) {
-            context.status = 400
-        }
-        updates.forEach((update) => topic[update] = context.request.body[update])
-        await topic.save()
-        context.status = 200
-        context.body = topic
-    } catch (e) {
-        console.log(e)
-        context.status = 400
+    if (!error) {
+      await Topic.findOneAndUpdate({ _id: _id }, { name, age, email })
+      context.body = 'success'
+    } else {
+      context.status = 400
+      context.body = error
     }
-
-    return;
+  } catch (e) {
+    context.status = 404
+    context.body = e
+  }
 })
 
 router.post('/topics', async (context) => {
-    const { name } = context.request.body
+  const { name, age, email } = context.request.body
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(20),
+    age: Joi.number().integer(),
+    email: Joi.string().min(10).max(30)
+  })
 
-    const newTopic = new Topic({ name })
+  const { error } = schema.validate({ name: name, age: age, email: email })
+  if (!error) {
+    const newTopic = new Topic({ name, age, email })
     const topic = await newTopic.save()
+    context.status = 201
+    context.body = topic
+  } else {
+    context.status = 400
+    context.body = error
+  }
+})
 
-    context.status = 201;
-    context.body = topic;
-
-    return;
-});
-
-module.exports = router;
+module.exports = router
